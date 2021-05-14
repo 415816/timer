acedb4616a3866ec40a59de0eeec6ad609b6b298
root <chefa@list.ru>
2021-05-14 12:54:46 +0300

    creat js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('sdsd');
+let time=5*60;
+let r = document.querySelector('span');
+let tmp = time;
+
+setInterval(function(){
+    let c = tmp--;
+    let m = (c/60) >> 0;
+    let s = (c - m * 60) + '';
+    r.textContent = m + ':' + (s.length > 1 ? '' : '0') + s;
+    tmp != 0 || (tmp=time);
+},1000);
